42f6618a42715055f8f61e222dc168e08cbf0ce8
park-gb <dev.gbpark@gmail.com>
2022-01-14 09:35:25 +0900

    Update

--- src/layouts/TopMenu.js
+++ src/layouts/TopMenu.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -7,10 +6,10 @@ const TopMenu = () => {
       <>
        <Nav  className="justify-content-center" variant="pills" >
           <Nav.Item>
-          <Nav.Link as={Link} to="/employee/profile" eventKey="link-1">인사</Nav.Link>
+          <Nav.Link as={Link} to="/employee/profile" eventKey="link-1" >인사</Nav.Link>
           </Nav.Item>   
           <Nav.Item>
-            <Nav.Link as={Link} to="/bizTrip/bizTripDetail" eventKey="link-2">출장</Nav.Link>
+            <Nav.Link as={Link} to="/bizTrip/bizTripDetail" eventKey="link-2" activeStyle={{color: "red"}}>출장</Nav.Link>
           </Nav.Item>       
           <Nav.Item>
             <Nav.Link as={Link} to="/admin/list" eventKey="link-3">Admin</Nav.Link>
