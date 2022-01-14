cb72bc40314a188f5dccb7caea7e2d5973faef2b
goaldae <goaldae@naver.com>
2022-01-14 09:27:24 +0900

    Add test register page and edit url working

--- src/routers/MainRouter.js
+++ src/routers/MainRouter.js
@@ -1,26 +1,31 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import authService from "../services/auth.service";
 import HomeRouter from "./HomeRouter";
 import Login from "../components/LoginForm";
+import Register from "../components/register.component";
 
 const MainRouter = () => { 
     const user = authService.getCurrentUser(); 
     let currLocation = window.location.href;
     currLocation = currLocation.split('http://localhost:3000')[1];
-
+    const history = useHistory();
     if(user){ //로그인 유저가 있을 때
       if(currLocation==='/'|| // 루트 디렉토리나 어드민이 아닌데 어드민 접근하면
       (currLocation.includes("/admin")&&user.roles.includes("ROLE_USER"))) currLocation = '/employee/profile' //employee profile 페이지로
-    } else { //없을 때
+    } else if(currLocation==='/register'){}
+    else { //없을 때
       currLocation = '/login'
     }
+    console.log(currLocation);
     return (      
       <div>
-        <Redirect to={currLocation}/>
+        {history.push(currLocation)}
+        {/* <Redirect to={currLocation}/> */}
                 
         <Switch>
           <Route path={["/employee", "/biztrip", "/admin"]} component={HomeRouter} />
           <Route exact path="/login" component={Login} />      
+          <Route exact path="/register" component={Register} />   
         </Switch>
       </div>
     );
